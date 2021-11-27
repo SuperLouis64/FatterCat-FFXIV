e0febb98780c9a10083669d6898aa09c6fcbb6e3
SuperLouis64 <68299724+SuperLouis64@users.noreply.github.com>
2021-11-27 13:18:29 -0600

    Update FatCatMount.ino
    got rid of some leftover code from when I was messing around. Should be cleaner now!

--- FatCatMount.ino
+++ FatCatMount.ino
@@ -1,22 +1,15 @@
 /* Fat Cat set-up
  *  Goal: When I sit, I fat cat
- *  Only mount when the motion is live (give it a couple of seconds)
- *  Touch sensor can be sat on to confirm?
- *  be able to stitch this into something ontop of the fat cat
+ *  Touch sensor can be sat on to press the numpad 7
+ *  Numpad 7 is the hotbar key I use for my mounts on FFXIV
  */
-#include <Wire.h>
-#include <I2Cdev.h>
-#include <Mouse.h>
 #include <Keyboard.h>
-#include <Joystick.h>
 
-int pirPin = 2; // the output of the PIR
-int pirStatus = 0; 
 
 bool isSitting;
 
 // touch sensor stuff
-const int SENSOR_PIN = 4;
+const int SENSOR_PIN = 4; // the pin that the TOUCH SENSOR is connected to
 int lastState = LOW;      // the previous state from the input pin
 int currentState;         // the current reading from the input pin
 
@@ -30,13 +23,6 @@ void setup()
 
 void loop() 
 {
-  /*
-  pirStatus = digitalRead(pirPin);
-  if(pirStatus == HIGH)
-    {
-      Serial.println("motion detected");
-    }
-  */
 
  // read the state of the the input pin:
   currentState = digitalRead(SENSOR_PIN);
@@ -49,7 +35,7 @@ void loop()
   if(currentState == HIGH && !isSitting)
     {
       Keyboard.press (231); // numpad 7
-      Keyboard.release (231);
+      Keyboard.release (231); // code for numpad 7
       isSitting = true;
     }
 
@@ -60,10 +46,11 @@ void loop()
       isSitting = false;
      }
 
-   Serial.println(isSitting);
 
   // save the the last state
   lastState = currentState;
     
+  // debug stuff
+  //   Serial.println(isSitting); 
 
 }
